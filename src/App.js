f0742561923dc471c798b069df75afbe848b3270
Knight <mayanks365@gmail.com>
2021-01-28 23:27:37 +0530

    updating with componentDidMount and Alternate State Initialize

--- src/App.js
+++ src/App.js
@@ -1,33 +1,24 @@
 import SeasonDisplay from './SeasonDisplay';
 import { Component } from 'react';
 
-// must extend subclass -> extends React.Component
-// must define render that return JSX
-class App extends Component{
-    constructor(props) {
-      super(props);
+class App extends Component {
+  
+  constructor(props) {
+    super(props);
+    this.state = { lat: null, errorMessage: '' };
 
-      // state must be assigned with name state
-      // can't assgin state by any other name
-      this.state = { lat: null, errorMessage: '' };
+  }
 
-      // this will run when we create instance
-      window.navigator.geolocation.getCurrentPosition(
-      ( position ) => { 
-        // only place we are calling setState!!!
-        // any time you change your setState will cause component  to re-render()
-        // this call-back will take some time to run, run after fetching position.
-        this.setState( { lat: position.coords.latitude });
-      },
-      ( err ) => this.setState( { errorMessage: err.message } )
+  // official React Documentation prefer not loading data inside constructor function
+  // if you keep you data loading code inside componentDidMount your code will look clean and easy to follow
+  componentDidMount(){
+    window.navigator.geolocation.getCurrentPosition(
+      position => this.setState( { lat: position.coords.latitude }),
+      err => this.setState( { errorMessage: err.message } )
     )
   }
 
-  // render method inside the component is called frequently
-  // react will re-render the component anytime you update the state object
-  // render will run second time to update the postion on screen
   render(){
-
     if( this.state.errorMessage && !this.state.lat ){
       return (
         <div className="App">
@@ -52,7 +43,7 @@ class App extends Component{
       <div className="App">
             <SeasonDisplay>
               <div>
-                <h1> Loading....</h1>
+                <h3> Loading....</h3>
               </div>
             </SeasonDisplay>
       </div>
